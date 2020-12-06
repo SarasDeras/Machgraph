eb9cf71591bba12ab9af8aa622d73576d489b13a
SarasDeras <52974802+SarasDeras@users.noreply.github.com>
2020-12-07 00:38:59 +0300

    Shaders

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 #define GLEW_STATIC
 #include "libs/include/glew.h"
 #include "libs/include/glfw3.h"
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
-    // Когда пользователь нажимает ESC, мы устанавливаем свойство WindowShouldClose в true,
-    // и приложение после этого закроется
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+// Shaders
+const GLchar* vertexShaderSource = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 position;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+                                   "}\0";
+
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 color;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\n\0";
+
+using namespace std;
+
+string readFile(const string& fileName){
+    stringstream Stream;
+    ifstream File;
+    File.exceptions(ifstream::badbit);
+    try {
+        File.open(fileName);
+        Stream << File.rdbuf();
+        File.close();
+        return Stream.str();
+    } catch (ifstream::failure e) {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+}
+
 
 int main() {
     //Инициализация GLFW
@@ -45,14 +79,97 @@ int main() {
 
     glfwSetKeyCallback(window, key_callback);
 
+    GLfloat vertices[] = {
+            0.5f,  0.5f, 0.0f,  // Верхний правый угол
+            0.5f, -0.5f, 0.0f,  // Нижний правый угол
+            -0.5f, -0.5f, 0.0f,  // Нижний левый угол
+            -0.5f,  0.5f, 0.0f   // Верхний левый угол
+    };
+    GLuint indices[] = {  // Помните, что мы начинаем с 0!
+            0, 1, 3,   // Первый треугольник
+            1, 2, 3    // Второй треугольник
+    };
+
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    //Шейдеры
+    string vert_str = readFile(R"(..\shaders\vert.txt)");
+    string frag_str = readFile(R"(..\shaders\frag.txt)");
+
+    //Вершинный шейдер
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char *src = vert_str.c_str();
+    GLint success;
+    GLchar infoLog[512];
+
+
+    glShaderSource(vertexShader, 1, &src, nullptr);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if(!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+        cout << "VertexShader: Failed\n"<< infoLog<< endl;
+    }
+
+    //Фрагментный шейдер
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    src = frag_str.c_str();
+    glShaderSource(fragmentShader, 1, &src, nullptr);
+    glCompileShader(fragmentShader);
+    if(!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+        cout << "fragmentShader: Failed\n" << infoLog<< endl;
+    }
+
+    //Соединяем шейдеры
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
         glfwSwapBuffers(window);
     }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glfwTerminate();
     return 0;
 }
