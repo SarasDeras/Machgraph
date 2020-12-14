1f9e51d7fccc907002b7250ab933ab1e45f036b4
SarasDeras <52974802+SarasDeras@users.noreply.github.com>
2020-12-14 14:28:13 +0300

    Release [FIX]

--- Model.h
+++ Model.h
@@ -11,10 +11,6 @@
 #include "libs/include/SOIL.h"
 #include "Shader.h"
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
---
+++ cmake-build-debug/CMakeFiles/MashGraph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MashGraph.dir/main.cpp.obj"
+  "MashGraph.exe"
+  "MashGraph.exe.manifest"
+  "MashGraph.pdb"
+  "libMashGraph.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MashGraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
